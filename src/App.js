677d6c3850f1afc244a09b47f183a000851887f4
Steve Babb <steve.babb@outlook.com>
2021-04-20 08:54:29 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>hello world</h1>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Utils/API.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
+export default {
+  getRandomDog: function() {
+    return axios.get("https://dog.ceo/api/breeds/image/random");
+  },
+  getDogsOfBreed: function(breed) {
+    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+  },
+  getBaseBreedsList: function() {
+    return axios.get("https://dog.ceo/api/breeds/list");
+  }
+};
