588cdaf9a6e420c7d8651f3307c7b2aecbc3e85d
Steve Babb <steve.babb@outlook.com>
2021-04-20 12:18:27 -0500

    add api call for random

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,17 +1,9 @@
 import axios from "axios";
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
+  getRandomEmployees: function() {
+    return axios.get("https://randomuser.me/api/?results=20");
   },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
-  }
 };
