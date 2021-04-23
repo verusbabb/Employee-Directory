7f1a4f39e5464aae4e258f8cddca2706f365a36e
Steve Babb <steve.babb@outlook.com>
2021-04-23 12:22:34 -0500

    add basic styling search box

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,18 +2,25 @@ import React from "react";
 
 export default function Form({ search, handleInputChange }) {
   return (
-    <form>
-      <div className="form-group">
-        <label for="search">search</label>
-        <input
-          name="search"
-          type="text"
-          className="form-control"
-          id="search"
-          onChange={handleInputChange}
-          value={search}
-        ></input>
+    <div>
+      <div className="input-group mb-4">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1">
+            Employee Search
+          </span>
+        </div>
+        <div className="col-sm-4">
+          <input
+            name="search"
+            type="text"
+            className="form-control"
+            id="search"
+            onChange={handleInputChange}
+            value={search}
+            placeholder="Enter employee first name"
+          ></input>
+        </div>
       </div>
-    </form>
+    </div>
   );
 }
