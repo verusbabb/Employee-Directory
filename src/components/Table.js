b25a98c2db7d4dfe97cb00672981f0db4c539a77
Steve Babb <steve.babb@outlook.com>
2021-04-23 09:40:17 -0500

    attempting useState

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,55 +1,49 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import API from "../Utils/API";
 import Tablebody from "./Tablebody";
 import moment from "moment";
-// import Tableheader from "./Tableheader";
-// import Tablebody from "./Tablebody";
-// import Tableheader from "./Tableheader";
 
-class Table extends Component {
-  state = {
-    employeeResult: [],
-  };
+function Table() {
+  const [employeeResult, setEmployeeResult] = useState();
 
   // When the component mounts, load the RANDOM EMPLOYEES to be displayed
-  componentDidMount() {
-    this.loadRandomEmployees();
-  }
+  // componentDidMount() {
+  //   loadRandomEmployees();
+  // };
 
-  loadRandomEmployees = () => {
+  useEffect(() => {
     API.getRandomEmployees().then((res) => {
-      this.setState({ employeeResult: res.data.results });
+      setEmployeeResult(res.data);
 
-      console.table(this.state.employeeResult);
+      console.table(employeeResult);
     });
-  };
+  }, []);
 
-  render() {
-    return (
-      <div className="table-responsive">
-        <table className="table table-striped table-bordered">
-          <thead className="bg-primary text-white ">
-            <tr>
-              <th scope="col">Picture</th>
-              <th scope="col">Name</th>
-              <th scope="col">Email</th>
-              <th scope="col">DOB</th>
-            </tr>
-          </thead>
-          {this.state.employeeResult.map((employee, index) => (
-            <Tablebody
-              key={index}
-              picture={employee.picture.thumbnail}
-              nameFirst={employee.name.first}
-              nameLast={employee.name.last}
-              email={employee.email}
-              dob={moment(employee.dob.date).format("L")}
-            />
-          ))}
-        </table>
-      </div>
-    );
-  }
+  // render() {
+  return (
+    <div className="table-responsive">
+      <table className="table table-striped table-bordered">
+        <thead className="bg-primary text-white ">
+          <tr>
+            <th scope="col">Picture</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+        {employeeResult.map((employee, index) => (
+          <Tablebody
+            key={index}
+            picture={employee.picture.thumbnail}
+            nameFirst={employee.name.first}
+            nameLast={employee.name.last}
+            email={employee.email}
+            dob={moment(employee.dob.date).format("L")}
+          />
+        ))}
+      </table>
+    </div>
+  );
 }
 
 export default Table;
