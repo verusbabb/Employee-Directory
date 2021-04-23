f46526a43914bd51e686080b68841f33cd397844
Steve Babb <steve.babb@outlook.com>
2021-04-23 08:31:01 -0500

    cleanup working code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Table from "./components/Table";
 
 function App() {
   return (
-    // <Router>
     <div className="row">
       <div className="container">
         <h1 className="text-center">Employee Directory</h1>
         <Table />
       </div>
     </div>
-
-    // </Router>
   );
 }
 
